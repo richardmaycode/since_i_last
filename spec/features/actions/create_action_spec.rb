3731b1ec8e746acc463a3a86f2e4aeaadd750459
Richard Wise <orsusbass@gmail.com>
2021-05-31 20:54:22 -0500

    wireframe create action feature spec

---
+++ spec/features/actions/create_action_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe "Create Action", type: :feature do
+  context "with valid attributes" do
+    xit "will create an action" do
+    end
+  end
+
+  context "with invalid attributes" do
+    xit "will not create an action" do
+    end
+  end
+end
