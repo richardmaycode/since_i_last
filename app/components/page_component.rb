aef87d8d902f70474a624e7aedf016520507fa7e
Richard Wise <richard.wise@hey.com>
2021-06-11 20:40:33 -0500

    create narraw page variant

--- app/components/page_component.rb
+++ app/components/page_component.rb
@@ -4,7 +4,8 @@ class PageComponent < ViewComponent::Base
   renders_one :header, Page::HeaderComponent
   renders_many :sections, Page::SectionComponent
 
-  def initialize(id:nil)
+  def initialize(id: nil, is_narrow: false)
     @id = id
+    @is_narrow = is_narrow
   end
 end
